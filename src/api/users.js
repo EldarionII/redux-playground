f450a9595ec0a3eff70cfa191567f3205e5a648d
Fedor Kostrov <kostrov.fedor@gmail.com>
2019-03-27 16:23:40 +0300

    added simple UI to test new user methods

--- src/api/users.js
+++ src/api/users.js
@@ -1,15 +1,19 @@
 import axios from 'axios'
 
 export class UsersApi {
-    static usersGet() {
-        return axios.get('https://localhost:3000/api/users')
-    }
+	static usersGet() {
+		return axios.get('https://localhost:3000/api/users')
+	}
 
-    static userCreate(user) {
-        return axios.post('https://localhost:3000/api/users', user)
-    }
+	static userCreate(user) {
+		return axios.post('https://localhost:3000/api/users', user)
+	}
 
-    static userDelete(userId) {
-        return axios.delete(`https://localhost:3000/api/users/${userId}`)
-    }
+	static userUpdate(userId, user) {
+		return axios.patch(`https://localhost:3000/api/users/${userId}`, user)
+	}
+
+	static userDelete(userId) {
+		return axios.delete(`https://localhost:3000/api/users/${userId}`)
+	}
 }
